4fb2612809931f6aa410a7c45d99e897a71dd435
Brenden <brenden.black@gov.bc.ca>
2018-01-07 21:20:21 -0800

    removing unused files

--- files/util/vagrant/firewall-reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-#Flush/remove all existing rules
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-sudo iptables -F
-sudo iptables -nL --line-numbers
--- files/util/vagrant/vagrant.bash_profile
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-export PS1='[\W:\u]\$ '
-cd /vagrant/
-echo "Your are here: $(pwd)"
-. ~/.bashrc
